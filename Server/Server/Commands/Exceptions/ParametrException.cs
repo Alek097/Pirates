83f5c4bf9ca3ac8129c6a0beddd1ae7b9ce49be6
Alek097 <petrovskii097@gmail.com>
2016-08-09 19:26:40 +0300

    Initial commit: added ParametrException

---
+++ Server/Server/Commands/Exceptions/ParametrException.cs
@@ -0,0 +1,22 @@
+﻿namespace Server.Commands.Exceptions
+{
+	#region Using
+	using System;
+	#endregion
+	class ParametrException : Exception
+	{
+		public override string StackTrace
+		{
+			get
+			{
+				return Environment.StackTrace;
+			}
+		}
+
+		public ParametrException(string message) :
+			base(message)
+		{
+			
+		}
+	}
+}
