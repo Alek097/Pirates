b0c6b88401535b560f100da233f2f1b9af573b1a
Alek097 <petrovskii097@gmail.com>
2016-08-11 13:50:11 +0300

    fix

--- Server/Server/Commands/HelpCommand.cs
+++ Server/Server/Commands/HelpCommand.cs
@@ -10,6 +10,7 @@
 	class HelpCommand : ServerCommand
 	{
 		private List<ServerCommand> commands;
+		private ParametrInformation[] parametrs = new ParametrInformation[] { new ParametrInformation("CommandName", "Name of commnd", Informations.ValueType.String, false) };
 		private const string line = "----------------------------------------------------";
 
 		public HelpCommand(List<ServerCommand> commands)
@@ -22,9 +23,7 @@
 		{
 			get
 			{
-				return new ParametrInformation[] {
-					new ParametrInformation("CommandName", "Name of commnd", Informations.ValueType.String, false)
-				};
+				return this.parametrs;
 			}
 		}
 
@@ -39,17 +38,17 @@
 
 		public override void StartCommand(params Parametr[] parametrs)
 		{
-			if(parametrs.Length > 0)
+			if (parametrs.Length > 0)
 			{
 				Parametr parametr = parametrs.FirstOrDefault(p => p.Name.ToLower() == "commandname");
 
-				if(parametr == null)
+				if (parametr == null)
 				{
 					throw new ParametrException(string.Format("Parametr with name {0} not found", this.Parametrs[0].Name));
 				}
 				else
 				{
-					ServerCommand command = this.commands.FirstOrDefault(c=>c.Name == (string)parametr.Value);
+					ServerCommand command = this.commands.FirstOrDefault(c => c.Name == (string)parametr.Value);
 
 					if (command == null)
 					{
