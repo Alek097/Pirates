d907e9beedbb2c925b8f249836a44a87dc72a229
Alek097 <petrovskii097@gmail.com>
2016-08-11 13:50:20 +0300

    Initial commit: added start StartCommand

---
+++ Server/Server/Commands/StartCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Server.Commands.Informations;
+
+namespace Server.Commands
+{
+	class StartCommand : ServerCommand
+	{
+		protected override ParametrInformation[] Parametrs
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public override InformationBuilder Information()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StartCommand(params Parametr[] parametrs)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
