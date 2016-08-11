ac0a3565b34c085b509d241a3dcc5031a2c5a4ce
Alek097 <petrovskii097@gmail.com>
2016-08-11 20:01:19 +0300

    Initial commit: inherited ServerException and add method to string

--- Server/Server/Commands/Exceptions/ParameterException.cs
+++ Server/Server/Commands/Exceptions/ParameterException.cs
@@ -1,24 +1,18 @@
 ﻿namespace Server.Commands.Exceptions
 {
-	#region Using
-	using System;
-	#endregion
-	class ParameterException : Exception
+	class ParameterException : ServerException
 	{
-		public override string StackTrace
-		{
-			get
-			{
-				return Environment.StackTrace;
-			}
-		}
-
 		public Parameter Parameter { get; private set; }
 
 		public ParameterException(string message, Parameter parameter)
 			: base(message)
 		{
 			this.Parameter = parameter;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("{0}\nParametr: {1}", base.ToString(), this.Parameter);
 		}
 	}
 }
