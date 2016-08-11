b90eb4d687cfc7ea6b7594e24753b3628b72f530
Alek097 <petrovskii097@gmail.com>
2016-08-11 21:56:29 +0300

    Initial commit: added parsing command and method Error() in Main

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -4,6 +4,8 @@
 	using System.Collections.Generic;
 	using Commands;
 	using System;
+	using System.Linq;
+	using Commands.Exceptions;
 	#endregion
 	class Program
 	{
@@ -12,13 +14,63 @@
 		{
 			AddCommands();
 
-			while(true)
+			while (true)
 			{
 				string command = Console.ReadLine();
 
-				if(command.ToLower() =="exit")
+				if (command.ToLower() == "exit")
 				{
 					break;
+				}
+				else if (string.IsNullOrWhiteSpace(command))
+				{
+					continue;
+				}
+				else
+				{
+					List<string> commandParts = command.Split(' ').ToList();
+
+					string commandName = commandParts[0];
+
+					commandParts.RemoveAt(0);
+
+					ServerCommand serverCommand = serverCommands.FirstOrDefault(c => c.Name.ToLower() == commandName.ToLower());
+
+					try
+					{
+
+						if (serverCommand == null)
+						{
+							throw new ServerCommandException(
+								string.Format("The command: {0} not found.", commandName),
+								commandName
+								);
+						}
+						else
+						{
+							List<Parameter> parameters = new List<Parameter>();
+
+							foreach (string item in commandParts)
+							{
+								string[] parametrParts = item.Split(':');
+
+								if (parametrParts.Length == 2 && !string.IsNullOrWhiteSpace(parametrParts[0]) && !string.IsNullOrWhiteSpace(parametrParts[1]))
+								{
+									parameters.Add(new Parameter(parametrParts[0], parametrParts[1]));
+								}
+								else
+								{
+									throw new ServerException(string.Format("Command syntax errorr in {0}.", item));
+								}
+							}
+
+							serverCommand.Start(parameters.ToArray());
+						}
+					}
+					catch (ServerException ex)
+					{
+						Error(ex);
+					}
 				}
 			}
 		}
@@ -27,6 +79,16 @@
 		{
 			serverCommands.Add(new HelpCommand(serverCommands));
 			serverCommands.Add(new StartCommand());
+		}
+
+		static void Error(ServerException ex)
+		{
+			Console.WriteLine();
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine(ex);
+			Console.WriteLine("Use the help command.");
+			Console.ResetColor();
+			Console.WriteLine();
 		}
 	}
 }
