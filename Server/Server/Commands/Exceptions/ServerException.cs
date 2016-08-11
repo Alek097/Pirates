caa7fa80f3110bb9ecdee8ba9fe2b54e8a0536f4
Alek097 <petrovskii097@gmail.com>
2016-08-11 19:45:35 +0300

    Initial commit: added ServerCommandException and ServerException

---
+++ Server/Server/Commands/Exceptions/ServerException.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Server.Commands.Exceptions
+{
+	#region Using
+	using System;
+	#endregion
+	class ServerException : Exception
+	{
+		public override string StackTrace
+		{
+			get
+			{
+				return Environment.StackTrace;
+			}
+		}
+
+		public ServerException(string message)
+			: base(message)
+		{
+
+		}
+
+		public override string ToString()
+		{
+			return string.Format("Type: {0}\n Message: {1}\n Stack trace:{2}", this.GetType(), base.Message, this.StackTrace);
+		}
+	}
+}
