fcf6598e6963902f307ea80f550057fa11cc2e5b
Alek097 <petrovskii097@gmail.com>
2016-08-13 21:28:24 +0300

    Initial commit: added Player

---
+++ Pirates.Model/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace Pirates.Model
+{
+	#region Using
+	using System;
+	#endregion
+	[Serializable]
+	public class Player
+	{
+		public Guid Id { get; set; }
+		public string Email { get; set; }
+		public string NickName { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string FullName { get; set; }
+
+		public Player(string nickName, string email, string firstName, string lastName)
+		{
+			this.NickName = nickName;
+			this.Email = email;
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.FullName = string.Format("{0} {1}", firstName, lastName);
+		}
+	}
+}
