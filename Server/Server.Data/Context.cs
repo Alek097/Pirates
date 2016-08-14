c9dc19373f9da74facb2b4ad0c909275eb888545
Alek097 <petrovskii097@gmail.com>
2016-08-14 15:58:32 +0300

    hotfix

--- Server/Server.Data/Context.cs
+++ Server/Server.Data/Context.cs
@@ -7,13 +7,23 @@
 	public class Context : DbContext
 	{
 		public DbSet<Player> Players { get; set; }
-		public DbSet<Ship> Ships { get; set; }
-		public DbSet<ShipCharacteristic> Characteristics { get; set; }
-		public DbSet<NPC> NPC { get; set; }
+		//public DbSet<Ship> Ships { get; set; }
+		//public DbSet<ShipCharacteristic> ShipCharacteristic { get; set; }
+		//public DbSet<NPC> NPC { get; set; }
 
-		protected override void OnModelCreating(DbModelBuilder modelBuilder)
-		{
-			
-		}
+		//protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		//{
+		//	modelBuilder.Entity<Player>()
+		//		.HasOptional(player => player.Ship)
+		//		.WithRequired(ship => ship.Captain);
+
+		//	modelBuilder.Entity<Ship>()
+		//		.HasMany(ship => ship.Team)
+		//		.WithOptional(npc => npc.Ship);
+
+		//	modelBuilder.Entity<Ship>()
+		//		.HasMany(ship => ship.Characteristic)
+		//		.WithRequired(characteristic => characteristic.Ship);
+		//}
 	}
 }
--- Server/Server.Data/Models/Player.cs
+++ Server/Server.Data/Models/Player.cs
@@ -2,7 +2,7 @@
 {
 	public class Player : Pirates.Model.Player
 	{
-		public virtual Ship Ship { get; set; }
+		//public virtual Ship Ship { get; set; }
 		public virtual Role Role { get; set; }
 	}
 }
