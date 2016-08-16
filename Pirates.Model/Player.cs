a330288b885436c38ee9057ef683ac3e7143ad0e
Alek097 <petrovskii097@gmail.com>
2016-08-16 17:52:43 +0300

    fix

--- Pirates.Model/Player.cs
+++ Pirates.Model/Player.cs
@@ -14,17 +14,13 @@
 		public string FullName { get; set; }
 
 		public Player(string nickName, string email, string firstName, string lastName)
-			: this()
 		{
+			this.Id = Guid.NewGuid();
 			this.NickName = nickName;
 			this.Email = email;
 			this.FirstName = firstName;
 			this.LastName = lastName;
 			this.FullName = string.Format("{0} {1}", firstName, lastName);
+		}
 	}
-	public Player()
-	{
-		this.Id = Guid.NewGuid();
-	}
-}
 }
