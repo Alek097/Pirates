e2ea48664df18d4775b3986d5b6d61e023f9ccbd
Alek097 <petrovskii097@gmail.com>
2016-08-19 18:34:40 +0300

    fix

--- Server/Server/Commands/Exceptions/FotalServerException.cs
+++ Server/Server/Commands/Exceptions/FotalServerException.cs
@@ -7,18 +7,13 @@
 	{
 		public new System.Exception InnerException { get; private set; }
 		public Log Log { get; private set; }
-		public FotalServerException(string message, System.Exception innerEx)
-			: base(message)
-		{
-			this.InnerException = innerEx;
-		}
-		public FotalServerException(string message)
-			: base(message)
-		{
-
-		}
-
 		public FotalServerException(string message, Log log)
+			: base(message)
+		{
+
+		}
+
+		public FotalServerException(string message, Log log , System.Exception innerEx)
 			: base(message)
 		{
 			this.Log = log;
