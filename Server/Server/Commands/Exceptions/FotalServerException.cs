874207fc92ace141199ed1a96d26e7b517b48ff9
Alek097 <petrovskii097@gmail.com>
2016-08-19 17:31:26 +0300

    Initial commit: added FotalServerException

---
+++ Server/Server/Commands/Exceptions/FotalServerException.cs
@@ -0,0 +1,20 @@
+﻿namespace Server.Commands.Exceptions
+{
+	#region Using
+	using System;
+	#endregion
+	class FotalServerException : ServerException
+	{
+		public new Exception InnerException { get; private set; }
+		public FotalServerException(string message, Exception innerEx)
+			: base(message)
+		{
+			this.InnerException = innerEx;
+		}
+		public FotalServerException(string message)
+			: base(message)
+		{
+
+		}
+	}
+}
