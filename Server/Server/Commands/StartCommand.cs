c00eeb57917e6a1031072dc372fa08b1c2aa209c
Alek097 <petrovskii097@gmail.com>
2016-08-19 14:15:52 +0300

    Initial commit: Added read TCP port

--- Server/Server/Commands/StartCommand.cs
+++ Server/Server/Commands/StartCommand.cs
@@ -2,10 +2,36 @@
 {
 	#region Using
 	using System.Collections.Concurrent;
-	using Server.Commands.Informations;
+	using Informations;
+	using System.Threading.Tasks;
+	using System.Net.Sockets;
+	using System.Text;
 	#endregion
 	class StartCommand : ServerCommand
 	{
+		private Task register = new Task(()=>
+			{
+				using (TcpClient newClient = new TcpClient("localhost", 80))
+				{
+					while (true)
+					{
+						byte[] bytes = new byte[newClient.ReceiveBufferSize];
+
+						using (NetworkStream tcpStream = newClient.GetStream())
+						{
+							tcpStream.Read(bytes, 0, newClient.ReceiveBufferSize);
+							
+						}
+
+						if(bytes.Length == 0)
+						{
+							continue;
+						}
+
+						string returnData = Encoding.UTF8.GetString(bytes);
+					}
+				}
+			});
 		public StartCommand()
 			: base("Start", "Starting server.", new ParameterInformation[0])
 		{
@@ -13,7 +39,8 @@
 
 		public override void Start(params Parameter[] parameters)
 		{
-
+			this.register.Start();
 		}
+
 	}
 }
